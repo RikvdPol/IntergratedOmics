47d301bb075fffaf589deb92426db9376a30b8ac
RikvandePol <rikvdpol93@gmail.com>
2022-04-12 11:21:47 +0200

    added logging to reader file

--- Code/Reader.py
+++ Code/Reader.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import argparse
 import logging
 
@@ -20,21 +19,17 @@ class Reader:
         """"
         Read a file and store in variable.
         """
-        with open(file) as f:
-            data = f.read()
-
-        for line in data:
-            print(line)
-
-    def test(self):
-        print("Hoi")
+        data = pd.read_csv(file, sep="\t")
+        print(data.head())
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", help="File to be read", type=str, required=True)
+    parser.add_argument("-head", help="Confirm if a header exists in the file", type=int, required=True)
     args = parser.parse_args()
     read = Reader()
     read.reader(args.f)
-    read.test()
     print("Input file: %s" % args.f)
+    logging.basicConfig(filename='../Logfiles/reader.log', filemode='w', format='%(asctime)s %(message)s')
+    logging.warning(f'Reading of {args.f} successful')
