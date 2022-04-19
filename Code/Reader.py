b595d5149e1c21fd5e223491e0931248c58237f3
unknown <rvandepol@bioinf.nl>
2022-04-19 23:10:00 +0200

    Added system seperator detection

--- Code/Reader.py
+++ Code/Reader.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import argparse
+import os
 import logging
 
 __author__ = "Rik van de Pol"
@@ -19,23 +19,24 @@ class Reader:
         """"
         Read a file and print the first five rows.
         """
+        sep = os.path.sep
         try:
             data = pd.read_csv(file, sep="\t")
-            logging.basicConfig(filename='../Logfiles/reader.log', filemode='a', format='%(asctime)s %(message)s')
+            logging.basicConfig(filename=f'..{sep}Logfiles{sep}reader.log', filemode='a+', format='%(asctime)s %(message)s')
             logging.warning(f'Reading of {file} successful')
             print(data.head())
         except FileNotFoundError as e:
-            logging.basicConfig(filename='../Logfiles/reader.log', filemode='a', format='%(asctime)s %(message)s')
+            logging.basicConfig(filename=f'..{sep}Logfiles{sep}reader.log', filemode='a+', format='%(asctime)s %(message)s')
             logging.warning(f'{e}: Reading of {file} unsuccessful')
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", help="File to be read", type=str, required=True)
-    parser.add_argument("-head", help="Confirm if a header exists in the file", type=int, required=True)
-    parser.add_argument("-output", help="Output file", type=str, required=True)
-    args = parser.parse_args()
-    read = Reader()
-    read.reader(args.f)
-    print("Input file: %s" % args.f)
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-f", help="File to be read", type=str, required=True)
+#     parser.add_argument("-head", help="Confirm if a header exists in the file", type=int, required=True)
+#     parser.add_argument("-output", help="Output file", type=str, required=True)
+#     args = parser.parse_args()
+#     read = Reader()
+#     read.reader(args.f)
+#     print("Input file: %s" % args.f)
 
