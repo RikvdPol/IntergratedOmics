f94438d58b7644d229178a7113721874f521d2b3
RikvandePol <rikvdpol93@gmail.com>
2022-04-20 10:44:18 +0200

    Completed simple elasticnet

--- Code/Main.py
+++ Code/Main.py
@@ -1,16 +1,28 @@
 import argparse
 import Reader
 import Elasticnet
+import sys
 
 
-if __name__ == "__main__":
+def main():
+    #Parse commandline arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", help="File to be read", type=str, required=True)
     parser.add_argument("-head", help="Confirm if a header exists in the file", type=int, required=True)
     parser.add_argument("-output", help="Output file", type=str, required=True)
     args = parser.parse_args()
+
+    #Read the data provided via the commandline
     read = Reader.Reader()
     data = read.reader(args.f)
+
+    #Run elasticnet
     algorithm = Elasticnet.Elasticnet(data, "BMI")
     algorithm.extract_labels()
+    model, cv = algorithm.define_model()
+    algorithm.evaluate_model(model, cv)
     print("Input file: %s" % args.f)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
