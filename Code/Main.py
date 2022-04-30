71f168b1c3a680b698a331abd2ec1ab8b6bb6d8d
Ruben0595 <76920198+Ruben0595@users.noreply.github.com>
2022-04-30 10:58:58 +0200

    main file improvements + XG OOP
    started working on the main file and making XG object oriented

--- Code/Main.py
+++ Code/Main.py
@@ -1,6 +1,7 @@
 import argparse
 import Reader
 import Elasticnet
+import xgboost_algorithm
 import sys
 
 
@@ -9,19 +10,47 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", help="File to be read", type=str, required=True)
     parser.add_argument("-head", help="Confirm if a header exists in the file", type=int, required=True)
-    parser.add_argument("-output", help="Output file", type=str, required=True)
+    parser.add_argument("-ML_type", help="Output file", type=str, required=True)
     args = parser.parse_args()
 
     #Read the data provided via the commandline
     read = Reader.Reader()
     data = read.reader(args.f)
 
+    if args.ML_type == "Elasticnet":
+        try: 
+            algorithm = Elasticnet.Elasticnet(data, 'BMI')
+        except:
+            print("Error: Elasticnet failed")
+            sys.exit(1)
+
+    elif args.ML_type == "XG":
+        try:
+            pass
+        except:
+            print("Error: XG failed")
+            sys.exit(1)
+
+        algorithm = xgboost_algorithm.XG(data, 'BMI')
+        algorithm.XG_boost(data, 'BMI')
+
+    elif args.ML_type == "etcetera":
+        print("etcetera")
+    
+    else:
+        print("Please specify a valid ML type")     #raise exception?
+        sys.exit()
+
+
+
     #Run elasticnet
+    '''
     algorithm = Elasticnet.Elasticnet(data, "BMI")
     algorithm.extract_labels()
     model, cv = algorithm.define_model()
     algorithm.evaluate_model(model, cv)
     print("Input file: %s" % args.f)
+    '''
 
 
 if __name__ == "__main__":
--- Code/xgboost_algorithm.py
+++ Code/xgboost_algorithm.py
@@ -7,29 +7,40 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 # load data
 
-dataset = pd.read_csv('Code/Data/Covariates.csv', delimiter="\s+")
-# split data into X and y
-print(dataset.BMI)
+class XG:
+    def __init__(self, file, labelname):
+        self.file = file
+        self.labelname = labelname
+        self.labels = None
 
-dataset.dropna(inplace = True)
-X = dataset[['Age', 'Gender']]
-Y = dataset['BMI']
 
-Y = Y.astype(int)
+    def XG_boost(self, file, labelname):
+        self.file = file
+        self.labelname = labelname
+        self.labels = None
+        #dataset = pd.read_csv('Data/Covariates.csv', delimiter="\s+")
+        # split data into X and y
+        dataset = file
 
-X = X.to_numpy()
-Y = Y.to_numpy()
+        dataset.dropna(inplace = True)
+        X = dataset[['Age', 'Gender']]
+        Y = dataset['BMI']
 
-# split data into train and test sets
-seed = 7
-test_size = 0.33
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
-# fit model no training data
-model = XGBRegressor()
-model.fit(X_train, y_train)
-# make predictions for test data
-y_pred = model.predict(X_test)
-predictions = [round(value) for value in y_pred]
-# evaluate predictions
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
+        Y = Y.astype(int)
+
+        X = X.to_numpy()
+        Y = Y.to_numpy()
+
+        # split data into train and test sets
+        seed = 7
+        test_size = 0.33
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+        # fit model no training data
+        model = XGBRegressor()
+        model.fit(X_train, y_train)
+        # make predictions for test data
+        y_pred = model.predict(X_test)
+        predictions = [round(value) for value in y_pred]
+        # evaluate predictions
+        accuracy = accuracy_score(y_test, predictions)
+        print("Accuracy: %.2f%%" % (accuracy * 100.0))
