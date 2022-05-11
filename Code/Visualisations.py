70a21f77892dfa69c456b41847af2ea4cf3860f6
RikvandePol <rikvdpol93@gmail.com>
2022-05-11 17:55:22 +0200

    Added logging

--- Code/Visualisations.py
+++ Code/Visualisations.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import Logging
 # import plotly.express as px
+import sys
 
 __author__ = "Rik van de Pol"
 __license__ = "MIT"
@@ -11,8 +12,17 @@ class Visualisations:
     def __init__(self, data):
         self.data = data
 
-    def boxplot(self): 
-        labels, data = self.data.keys(), self.data.values()
+    def boxplot(self):
+        logs = Logging.Logging()
+        try: 
+            labels, data = self.data.keys(), self.data.values()
+            msg = (f"Algorithm name and data succesfully extracted")
+            logs.create_logs(self.__class__.__name__, msg)
+
+        except AttributeError as e:
+            msg = f"{e}: Data provided not in dicionary"
+            logs.create_logs(self.__class__.__name__, msg)
+            sys.exit(0)
 
         plt.boxplot(data)
         plt.xticks(range(1, len(labels) + 1), labels)
