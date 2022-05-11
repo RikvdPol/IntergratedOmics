4230fda658a36ae9ea5c14a9887d24991689b40d
RikvandePol <rikvdpol93@gmail.com>
2022-05-11 17:44:39 +0200

    Added documentation to split_data method

--- Code/Elasticnet.py
+++ Code/Elasticnet.py
@@ -35,10 +35,16 @@ class Elasticnet:
         self.file = self.file.drop([self.labelname, "Pseudo", "Antibody_batch"], axis=1)
 
     def split_data(self, test_size=0.3, random_state=None):
-        #splits the model. test.size determines the proportion of data
-        #to be used as test data, the remaining us used for training. Random state is automatically set to None
-        #The user can provide any integer to this, which ensures the same distribution of training and test data as long 
-        #as the number remains the same.
+        """
+        Split the data intro training and test data, and the labels intro training and test labels.
+        The split is decided by the percentage of data to be used for testing, provided by a number between
+        0 and 1. By defeault this number is set to 0.3, which means 30% of the data provided will be used
+        for testing, and 70% will be used for training. In addition, the split function expects a random state.
+        Providing a number will result in a specific random state, meaning that the same data is used for training
+        and testing every single time if a number is provided, otherwise this will be random everytime. 
+        This is random because is should not matter to the model which data it gets, it should always be comparable
+        to previous instances.
+        """
         X_train, X_test, y_train, y_test = train_test_split(self.file,
                                                             self.labels,
                                                             test_size=test_size,
@@ -64,5 +70,4 @@ class Elasticnet:
         scores = np.absolute(scores)
         return scores
         # print('Mean MAE: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
-        # print("All scores:", scores)
 
