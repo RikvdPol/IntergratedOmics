24d7cc6741f3a8d03f1140df5127112334096d2a
Maryam Jalali <94684581+farimahJl@users.noreply.github.com>
2022-05-17 12:00:25 +0200

    XGBoost  Module coding, small template

--- xgboost.py
+++ xgboost.py
@@ -1,7 +1,17 @@
-import tensorflow as tf
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-iris = load_iris()
+import xgboost
+print(xgboost.__version__)
+from xgboost import XGBRegressor
+# create an xgboost regression model
+model = XGBRegressor(
+    n_estimators=1000, 
+    max_depth=7, 
+    eta=0.1, 
+    subsample=0.7, 
+    colsample_bytree=0.8,
+)
+model.fit(X, y)
+plt.scatter(X, y, color='teal', edgecolors='black', label='Train')
+plt.plot(X, model.predict(X), color='orange',label='XGBoost regressor')
+plt.title('XGBoost Regression')
+plt.legend()
+plt.show()
