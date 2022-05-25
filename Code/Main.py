9ecd653e1f5dfe954fefd8b0ce4eed248c0ffd01
Ruben0595 <76920198+Ruben0595@users.noreply.github.com>
2022-05-25 11:33:38 +0200

    starting to clean main

--- Code/Main.py
+++ Code/Main.py
@@ -10,6 +10,8 @@ import xgboost_algorithm
 # import Preprocessing
 # import Recommendation
 
+models_dict = {'elasticnet': Elasticnet.Elasticnet, 'xgboost': xgboost_algorithm.XG}
+
 def main():
     # Parse commandline arguments
     parser = argparse.ArgumentParser()
@@ -21,25 +23,22 @@ def main():
     # Read the data provided via the commandline
     read = Reader.Reader(args.f, args.head)
     data = read.reader()
-
-
-# TODO upper en lower inputs
-# TODO let user define which labels are of interest
-
-
-    algorithm = Elasticnet.Elasticnet(data, 'BMI')
-    algorithm.extract_labels()
     X_train, X_test, y_train, y_test = algorithm.split_data()
-    elastic_model = algorithm.train_model(X_train, y_train)
-    predictions = algorithm.predict(elastic_model, X_test)
-    model, cv = algorithm.define_model()
-    scores = algorithm.evaluate_model(model, cv)
-    metrics = Metrics.Metrics(y_test, predictions)
-    r2 = metrics.r_squared()
-    mse = metrics.mean_squared_error()
-    mae = metrics.mean_absolute_error()
-    rmse = metrics.root_mean_squared_error()
-    print("Input file: %s" % args.f)
+
+    for model in models_dict:
+        print(models_dict[model])
+        algorithm = models_dict[model](data, 'BMI')
+        algorithm.extract_labels()
+        elastic_model = algorithm.train_model(X_train, y_train)
+        predictions = algorithm.predict(elastic_model, X_test)
+        model, cv = algorithm.define_model()
+        scores = algorithm.evaluate_model(model, cv)
+        metrics = Metrics.Metrics(y_test, predictions)
+        r2 = metrics.r_squared()
+        mse = metrics.mean_squared_error()
+        mae = metrics.mean_absolute_error()
+        rmse = metrics.root_mean_squared_error()
+        print("Input file: %s" % args.f)
             
     algorithm = xgboost_algorithm.XG(data, 'BMI')
     algorithm.extract_labels()
