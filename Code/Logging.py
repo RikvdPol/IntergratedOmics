bb0acf861a112c48b7807e23a14358d03e40f9f3
RikvdPol <rikvdpol93@gmail.com>
2022-06-10 22:04:24 +0200

    Improved file format

--- Code/Logging.py
+++ Code/Logging.py
@@ -1,7 +1,6 @@
 import logging
 from pathlib import Path
 
-
 __author__ = "Rik van de Pol"
 __license__ = "MIT"
 __email__ = "rikvdpol93@gmail.com"
@@ -15,7 +14,7 @@ class Logging:
     def create_logs(self, classname, msg):
         path = Path(f"../Logfiles/{classname}.log")
         logging.basicConfig(filename=path,
-                                filemode='a+',
-                                format='%(asctime)s %(message)s',
-                                force=True)
+                            filemode='a+',
+                            format='%(asctime)s %(message)s',
+                            force=True)
         logging.warning(msg)
